5df56b18821fd8d505b94ae7538c69f551004f33
shoukath-manarath <manarath524@gmail.com>
2022-07-06 10:22:37 +0530

    twig related commit

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,6 +21,16 @@ class QuestionController
      */
     public function showpage($slug)
     {
-        return new Response(sprintf('Future page to show a question "%s"!',ucwords(str_replace('-',' ',$slug))));
+        $answers = [
+            'fake answer 1',
+            'fake answer 2',
+            'fake answer 3',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-',' ',$slug)),
+            'answers' => $answers
+        ]);
+        //return new Response(sprintf('Future page to show a question "%s"!',ucwords(str_replace('-',' ',$slug))));
     }
 }
