b5d39f80c9c15be6c1c6981a847fa51ac665e973
shoukath-manarath <manarath524@gmail.com>
2022-07-11 12:09:09 +0530

    all

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -24,16 +26,21 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function showpage($slug)
+    public function showpage($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
     {
         $answers = [
-            'fake answer 1',
-            'fake answer 2',
-            'fake answer 3',
+            'fake answer one',
+            'fake answer two',
+            'fake answer three',
         ];
 
+        $questionText = "Shoukath here I've been turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
+        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser){
+            return $markdownParser->transformMarkdown($questionText);
+        });
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-',' ',$slug)),
+            'questionText' => $parsedQuestionText,
             'answers' => $answers
         ]);
     }
